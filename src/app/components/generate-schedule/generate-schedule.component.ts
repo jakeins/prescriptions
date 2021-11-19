bd1f057db94e02cbf49dd55ad641d263da110373
Max Nazarenko <maksyrup@gmail.com>
2021-11-19 13:23:03 +0200

    Finish treatment form

--- src/app/components/generate-schedule/generate-schedule.component.ts
+++ src/app/components/generate-schedule/generate-schedule.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ITreatment, IUserPermission } from 'src/app/models/shared';
 import { GodDataService } from 'src/app/services/god-data.service';
 
 @Component({
@@ -31,7 +32,7 @@ export class GenerateScheduleComponent implements OnInit {
     this.form = this.fb.group({
       treatmentReason: ['Sore throat', Validators.required],
 
-      medicationName: ['Tylenol', Validators.required],
+      medicationName: ['Tylenol 500mg pill', Validators.required],
 
       startingFrom: [tomorrow, Validators.required],
       days: [7, Validators.required],
@@ -46,17 +47,17 @@ export class GenerateScheduleComponent implements OnInit {
       doctorCanTake: {value: false, disabled: false},
       doctorCanEdit: {value: true, disabled: true},
 
-      patientEmail: ['', Validators.required],
+      patientEmail: '',
       patientCanWatch: {value: true, disabled: false},
       patientCanTake: {value: false, disabled: false},
       patientCanEdit: {value: false, disabled: true},
 
-      caregiverEmail: ['', Validators.required],
+      caregiverEmail: '',
       caregiverCanWatch: {value: true, disabled: true},
       caregiverCanTake: {value: true, disabled: false},
       caregiverCanEdit: {value: false, disabled: true},
 
-      responsibleEmail: ['', Validators.required],
+      responsibleEmail: '',
       responsibleCanWatch: {value: true, disabled: true},
       responsibleCanTake: {value: false, disabled: false},
       responsibleCanEdit: {value: false, disabled: true},
@@ -73,7 +74,99 @@ export class GenerateScheduleComponent implements OnInit {
 
   generate() {
     console.log(this.form);
-  }
 
+    const fval = this.form.getRawValue();
+
+    const dayTakes: [hours: number, minutes: number][] = [];
+
+    [ fval.morningTakeTime,
+      fval.dayTakeTime,
+      fval.eveningTakeTime ].forEach(xDayTime => {
+      if (xDayTime) {
+        const hours = xDayTime.getHours();
+        const mins = xDayTime.getMinutes();
+        dayTakes.push([ hours, mins]);
+      }
+    });
+
+    const scheduleTakes = this.godDataService.GenerateSimpleSchedule(
+      fval.startingFrom,
+      fval.days,
+      fval.everyNthDay,
+      dayTakes
+    );
+
+    console.log('scheduleTakes', scheduleTakes);
+
+    const permissions: IUserPermission[] = [];
+
+
+    if (fval.doctorEmail) {
+      permissions.push({
+        login: fval.doctorEmail,
+        permission: {
+            canWatch: fval.doctorCanWatch,
+            canTake: fval.doctorCanTake,
+            canEdit: fval.doctorCanEdit,
+          }
+      });
+    }
+
+    if (fval.patientEmail) {
+      permissions.push({
+        login: fval.patientEmail,
+        permission: {
+            canWatch: fval.patientCanWatch,
+            canTake: fval.patientCanTake,
+            canEdit: fval.patientCanEdit,
+          }
+      });
+    }
+
+    if (fval.caregiverEmail) {
+      permissions.push({
+        login: fval.caregiverEmail,
+        permission: {
+            canWatch: fval.caregiverCanWatch,
+            canTake: fval.caregiverCanTake,
+            canEdit: fval.caregiverCanEdit,
+          }
+      });
+    }
+
+    if (fval.responsibleEmail) {
+      permissions.push({
+        login: fval.responsibleEmail,
+        permission: {
+            canWatch: fval.responsibleCanWatch,
+            canTake: fval.responsibleCanTake,
+            canEdit: fval.responsibleCanEdit,
+          }
+      });
+    }
+
+    console.log(permissions);
+
+    const treatment: ITreatment = {
+      id: 0,
+      author: fval.doctorEmail,
+      name: fval.treatmentReason,
+      medications: [
+        {
+          guid: '',
+          name: fval.medicationName,
+          schedule: {
+            guid: '',
+            summary: '',
+            takes: scheduleTakes
+          }
+        }
+      ],
+      userPermissions: permissions
+    };
+
+    console.log(treatment);
+
+  }
 
 }
